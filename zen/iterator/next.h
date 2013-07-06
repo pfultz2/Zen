e46340e1a4482f2a698737de01bce5e729d625f6
Paul <pfultz2@yahoo.com>
2013-07-06 18:27:37 -0400

    Fix bug with function builder not adding the correct reference type

---
+++ zen/iterator/next.h
@@ -0,0 +1,14 @@
+//
+//  next.h
+//  zen
+//
+//  Created by Paul Fultz II on 6/29/13.
+//  Copyright (c) 2013 Paul Fultz II. All rights reserved.
+//
+
+#ifndef zen_next_h
+#define zen_next_h
+
+
+
+#endif
