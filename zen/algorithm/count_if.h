26d858eb88f1998ead6465406baa21b8086879b2
Paul <pfultz2@yahoo.com>
2013-07-06 18:46:48 -0400

    Add tests for count_if

--- zen/algorithm/count_if.h
+++ zen/algorithm/count_if.h
@@ -18,11 +18,30 @@
 
 namespace zen { 
 
-ZEN_FUNCTION_PIPE_OBJECT((count_if)(r, f)
+ZEN_FUNCTION_PIPE_OBJECT((count_if)(auto r, f)
         if (is_sequence<r>)(boost::fusion::count_if(r, f))
         else if (is_range<r>)(std::count_if(boost::begin(r), boost::end(r), f))
     )
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+#include <zen/algorithm/detail/is_odd.h>
+
+
+ZEN_TEST_CASE(count_if_test)
+{
+    std::vector<int> v = boost::assign::list_of(1)(2)(3);
+    
+    ZEN_TEST_EQUAL(2, zen::count_if(v, is_odd()));
+    
+    ZEN_TEST_EQUAL(2, zen::count_if(boost::fusion::make_vector(1,2,3), is_odd()));
+}
+
+#endif
 
 #endif
