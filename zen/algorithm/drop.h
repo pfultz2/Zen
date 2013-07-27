b4f6a5ca0351b3167eb4ba5ca1be5160dbb5d0ce
Paul <pfultz2@yahoo.com>
2013-07-27 18:15:47 -0400

    Add tests for drop

--- zen/algorithm/drop.h
+++ zen/algorithm/drop.h
@@ -13,11 +13,28 @@
 
 namespace zen { 
 
-ZEN_FUNCTION_PIPE_OBJECT((drop)(r, n)
-    if (is_range<r>)(make_iterator_range(detail::iterator_at(r), boost::end(r)))
+ZEN_FUNCTION_PIPE_OBJECT((drop)(auto r, n)
+    if (is_range<r>)(make_iterator_range(detail::iterator_at(r, n), boost::end(r)))
 
 )
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <zen/algorithm/equal.h>
+#include <boost/assign.hpp>
+#include <vector>
+
+ZEN_TEST_CASE(drop_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(1)(2)(3);
+    std::vector<int> v2 = boost::assign::list_of(2)(3);
+    
+    ZEN_TEST_CHECK(zen::equal(v1 | zen::drop(1), v2));
+    ZEN_TEST_CHECK(zen::equal(zen::drop(v1, 1), v2));
+}
+
+#endif
 
 #endif
