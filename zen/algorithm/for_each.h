029fa0d2c854d8ed8f40dbc58944f408cbe99e1e
Paul Fultz II <pfultz2@yahoo.com>
2013-07-27 21:49:42 -0500

    Add test for for_each

--- zen/algorithm/for_each.h
+++ zen/algorithm/for_each.h
@@ -18,12 +18,36 @@
 
 namespace zen { 
 
-ZEN_FUNCTION_PIPE_OBJECT((for_each)(r, f)
+ZEN_FUNCTION_PIPE_OBJECT((for_each)(auto r, f)
         if(is_range<r>)(std::for_each(boost::begin(r), boost::end(r), f))
         else if(is_sequence<r>)(boost::fusion::for_each(r, f))
     )
 
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+#include <zen/algorithm/equal.h>
+#include <zen/algorithm/detail/increment.h>
+
+
+ZEN_TEST_CASE(for_each_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(1)(2);
+    std::vector<int> v2 = boost::assign::list_of(2)(3);
+    boost::fusion::vector<int,int> s1(1,2);
+    boost::fusion::vector<int,int> s2(2,3);
+
+    v1 | zen::for_each(increment());
+    s1 | zen::for_each(increment());
+    ZEN_TEST_EQUAL(v1, v2);
+    ZEN_TEST_EQUAL(s1, s2);
+}
+
+#endif
 
 #endif
