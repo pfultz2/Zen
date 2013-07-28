3ad88eaedb9cf85a9863a402898bd45ac1ab6033
Paul Fultz II <pfultz2@yahoo.com>
2013-07-27 22:12:17 -0500

    Add increment function object for testing purporses

---
+++ zen/algorithm/detail/increment.h
@@ -0,0 +1,28 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    test.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZEN_GUARD_DETAIL_INCREMENT_H
+#define ZEN_GUARD_DETAIL_INCREMENT_H
+
+struct increment
+{
+    template<typename T>
+    void operator()(T& t) const
+    {
+        ++t;
+    }
+
+    template<typename T, typename U>
+    void operator()(const T&, U& u) const
+    {
+        ++u;
+    }
+};
+
+
+
+#endif
