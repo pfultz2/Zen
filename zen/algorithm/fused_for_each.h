3fc7bee34b18582b299513287088c868ec0ab386
Paul Fultz II <pfultz2@yahoo.com>
2013-07-27 22:12:38 -0500

    Add tests for fused_for_each

--- zen/algorithm/fused_for_each.h
+++ zen/algorithm/fused_for_each.h
@@ -19,4 +19,28 @@ ZEN_FUNCTION_PIPE_OBJECT((fused_for_each)(auto r, f)
 
 }
 
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <map>
+#include <zen/algorithm/equal.h>
+#include <zen/algorithm/detail/increment.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/fusion/adapted/std_pair.hpp>
+
+ZEN_FUNCTION_CLASS((fused_increment)(const x, y)(++(*y)))
+
+ZEN_TEST_CASE(fused_for_each_test)
+{
+    std::map<int, boost::shared_ptr<int> > m1 = boost::assign::map_list_of(0, boost::make_shared<int>(1))(1, boost::make_shared<int>(2));
+    std::map<int, boost::shared_ptr<int> > m2 = boost::assign::map_list_of(0, boost::make_shared<int>(2))(1, boost::make_shared<int>(3));
+
+    m1 | zen::fused_for_each(fused_increment());
+    ZEN_TEST_EQUAL(m1, m2);
+}
+
+#endif
+
 #endif
