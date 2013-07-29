ead2d3ab1a5586e107ca622b148291f3dacaeace
Paul Fultz II <pfultz2@yahoo.com>
2013-07-29 18:23:13 -0500

    Add test for take

--- zen/algorithm/take.h
+++ zen/algorithm/take.h
@@ -13,11 +13,28 @@
 
 namespace zen { 
 
-ZEN_FUNCTION_PIPE_OBJECT((take)(r, n)
-    if (is_range<r>)(make_iterator_range(boost::begin(r), detail::iterator_at(r)))
+ZEN_FUNCTION_PIPE_OBJECT((take)(auto r, n)
+    if (is_range<r>)(make_iterator_range(boost::begin(r), detail::iterator_at(r, n)))
 
 )
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <zen/algorithm/equal.h>
+#include <boost/assign.hpp>
+#include <vector>
+
+ZEN_TEST_CASE(take_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(1)(2)(3);
+    std::vector<int> v2 = boost::assign::list_of(1)(2);
+    
+    ZEN_TEST_CHECK(zen::equal(v1 | zen::take(2), v2));
+    ZEN_TEST_CHECK(zen::equal(zen::take(v1, 2), v2));
+}
+
+#endif
 
 #endif
