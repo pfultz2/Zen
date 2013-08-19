89dd4210278ec291ec6de6558c69dfa5bf63a849
Paul Fultz II <pfultz2@yahoo.com>
2013-08-19 00:29:19 -0500

    Add algorithm for at_c

--- zen/algorithm/at_c.h
+++ zen/algorithm/at_c.h
@@ -9,12 +9,53 @@
 #define ZEN_GUARD_ALGORITHM_AT_C_H
 
 #include <boost/fusion/sequence/intrinsic/at_c.hpp>
+#include <zen/function/builder.h>
 
 namespace zen { 
 
-// TODO: Make pipable
-using boost::fusion::at_c;
+namespace detail {
+
+template<int N>
+struct at_c_base
+{
+    ZEN_FUNCTION_CLASS((apply_base)(auto seq) if (is_sequence<seq>)(boost::fusion::at_c<N>(seq)));
+    typedef pipable_adaptor<apply_base> apply;
+};
+    
+}
+
+template<int N, class S>
+typename zen::result_of<typename detail::at_c_base<N>::apply(S)>::type
+at_c(S& s)
+{
+    return typename detail::at_c_base<N>::apply()(s);
+}
+
+template<int N>
+typename detail::at_c_base<N>::apply
+at_c()
+{
+    return typename detail::at_c_base<N>::apply();
+}
 
 }
+
+
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+
+
+ZEN_TEST_CASE(at_c_test)
+{
+    boost::fusion::vector<int, int, int> v(1, 2, 3);
+    ZEN_TEST_EQUAL(zen::at_c<1>(v), 2);
+    ZEN_TEST_EQUAL(v | zen::at_c<1>(), 2);
+}
+
+#endif
 
 #endif
