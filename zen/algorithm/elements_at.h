2b10367c13a1d3db144426e660d3ab274eff3b4a
Paul <pfultz2@yahoo.com>
2013-09-07 15:08:13 -0400

    Add elements_at algorithm

--- zen/algorithm/elements_at.h
+++ zen/algorithm/elements_at.h
@@ -8,13 +8,45 @@
 #ifndef ZEN_GUARD_ALGORITHM_ELEMENT_AT_H
 #define ZEN_GUARD_ALGORITHM_ELEMENT_AT_H
 
-namespace zen { namespace algorithm {
+#include <zen/algorithm/at.h>
+#include <zen/algorithm/transform.h>
+#include <zen/function/partial.h>
 
-class element_at
+namespace zen { 
+
+namespace detail {
+
+ZEN_FUNCTION_OBJECT((select_at)(n, auto r)(zen::at(r, n)))
+
+}
+
+ZEN_FUNCTION_PIPE_OBJECT((elements_at)(auto r, n)
+        if (is_range<r>)
+        (
+            r | zen::transform(zen::partial(detail::select_at)(n))
+        )
+    )
+}
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+
+
+ZEN_TEST_CASE(elemets_at_test)
 {
+    std::vector<int> v1 = boost::assign::list_of(1)(1)(1);
+    std::vector<std::vector<int> > v;
+    v.push_back(v1);
+    v.push_back(v1);
+    v.push_back(v1);
+    
+    ZEN_TEST_EQUAL(v1, zen::elements_at(v, 1));
+    ZEN_TEST_EQUAL(v1, v | zen::elements_at(1));
+}
 
-};
-
-}}
+#endif
 
 #endif
