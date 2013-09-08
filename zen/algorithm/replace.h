38ebc8f6c1d467203bb5ee744519973989eae9b2
Paul <pfultz2@yahoo.com>
2013-09-08 00:53:22 -0400

    Add tests for replace and replace_if

--- zen/algorithm/replace.h
+++ zen/algorithm/replace.h
@@ -9,13 +9,13 @@
 #define ZEN_GUARD_ALGORITHM_REPLACE_H
 
 #include <zen/algorithm/transform.h>
-#include <boost/bind.hpp>
+#include <zen/function/partial.h>
 
 namespace zen { 
 
 namespace detail {
 
-ZEN_FUNCTION_CLASS((replacer)(auto x, auto old_value, auto new_value)
+ZEN_FUNCTION_CLASS((replacer)(auto old_value, auto new_value, auto x)
     (
         x == old_value ? new_value : x
     ))
@@ -23,10 +23,30 @@ ZEN_FUNCTION_CLASS((replacer)(auto x, auto old_value, auto new_value)
 }
 
 ZEN_FUNCTION_PIPE_OBJECT((replace)(auto r, auto old_value, auto new_value)
-    if (is_range_or_sequence<r>)(zen::transform(r, boost::bind(detail::replacer(), _1, old_value, new_value)))
-
+    if (is_range_or_sequence<r>)
+    (
+        zen::transform(r, partial(detail::replacer())(old_value, new_value))
     )
+)
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+
+
+ZEN_TEST_CASE(replace_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(0)(1)(2)(3)(4);
+    std::vector<int> v2 = boost::assign::list_of(0)(1)(99)(3)(4);
+    
+    ZEN_TEST_EQUAL(v2, zen::replace(v1, 2, 99));
+    ZEN_TEST_EQUAL(v2, v1 | zen::replace(2, 99));
+}
+
+#endif
 
 #endif
--- zen/algorithm/replace_if.h
+++ zen/algorithm/replace_if.h
@@ -9,13 +9,13 @@
 #define ZEN_GUARD_ALGORITHM_REPLACE_IF_H
 
 #include <zen/algorithm/transform.h>
-#include <boost/bind.hpp>
+#include <zen/function/partial.h>
 
 namespace zen { 
 
 namespace detail {
 
-ZEN_FUNCTION_CLASS((replacer_if)(auto x, auto f, auto new_value)
+ZEN_FUNCTION_CLASS((replacer_if)(auto f, auto new_value, auto x)
     (
         f(x) ? new_value : x
     ))
@@ -23,10 +23,31 @@ ZEN_FUNCTION_CLASS((replacer_if)(auto x, auto f, auto new_value)
 }
 
 ZEN_FUNCTION_PIPE_OBJECT((replace_if)(auto r, f, auto new_value)
-    if (is_range_or_sequence<r>)(zen::transform(r, boost::bind(detail::replacer_if(), _1, f, new_value)))
+    if (is_range_or_sequence<r>)
+        (
+            zen::transform(r, partial(detail::replacer_if())(f, new_value))
+        )
 
     )
 
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <boost/fusion/container/vector.hpp>
+#include <zen/function/always.h>
+
+ZEN_TEST_CASE(replace_if_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(0)(1)(2)(3)(4);
+    std::vector<int> v2 = boost::assign::list_of(99)(99)(99)(99)(99);
+    
+    ZEN_TEST_EQUAL(v2, zen::replace_if(v1, zen::always(true), 99));
+    ZEN_TEST_EQUAL(v2, v1 | zen::replace_if(zen::always(true), 99));
+}
+
+#endif
 
 #endif
