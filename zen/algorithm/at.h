d101d16f167ab5a43910f0af15bd9ca1035cfd46
Paul Fultz II <pfultz2@yahoo.com>
2013-09-28 14:40:15 -0500

    Fix bugs in iterator_at, and added more tests

--- zen/algorithm/at.h
+++ zen/algorithm/at.h
@@ -23,15 +23,40 @@ namespace zen {
 
 namespace detail {
 
+template<class Iterator>
+Iterator iterator_at_back(Iterator it, Iterator start, Iterator stop)
+{
+    while(it != stop)
+    {
+        ++it;
+        ++start;
+    }
+    return start;
+}
+
+template<class Range, class I>
+bool index_in_range(const Range& r, I n)
+{
+    long size = boost::distance(r);
+    if (n < 0) return n >= -size;
+    else return n < size;
+}
+
 template<class R, class I>
 typename boost::range_iterator<R>::type
 iterator_at(R& r, I n)
 {
-    // TODO: Assert index is in range
-//    typename boost::range_iterator<const R>::type it;
-//    if (n < 0) it = boost::end(r);
-//    else it = boost::begin(r);
-    return zen::next(((n > 0) ? boost::begin(r) : boost::end(r)), n);
+    typedef typename boost::range_iterator<R>::type iterator;
+    if (has_iterator_traversal<iterator, boost::bidirectional_traversal_tag>::value)
+    {
+        ZEN_ASSERT(index_in_range(r, n));
+        return zen::next(((n >= 0) ? boost::begin(r) : boost::end(r)), n);
+    }
+    else
+    {
+        if (n >= 0) return zen::next(boost::begin(r), n);
+        else return iterator_at_back(zen::next(boost::begin(r), -(long)n), boost::begin(r), boost::end(r));
+    }
 }
 
 }
@@ -51,16 +76,33 @@ ZEN_FUNCTION_PIPE_OBJECT((at)(auto r, n)
 #include <zen/test.h>
 #include <boost/assign.hpp>
 #include <vector>
+#include <list>
+#include <boost/container/slist.hpp>
 #include <boost/fusion/container/vector.hpp>
 
 
 ZEN_TEST_CASE(at_test)
 {
-    std::vector<int> v1 = boost::assign::list_of(0)(1)(2)(3)(4);
+    std::vector<int> v1 = boost::assign::list_of(9)(1)(2)(3)(4);
     
+    ZEN_TEST_EQUAL(9, v1 | zen::at(0));
     ZEN_TEST_EQUAL(2, v1 | zen::at(2));
     ZEN_TEST_EQUAL(4, v1 | zen::at(-1));
     ZEN_TEST_EQUAL(3, v1 | zen::at(-2));
+
+    std::list<int> l1 = boost::assign::list_of(9)(1)(2)(3)(4);
+
+    ZEN_TEST_EQUAL(9, l1 | zen::at(0));
+    ZEN_TEST_EQUAL(2, l1 | zen::at(2));
+    ZEN_TEST_EQUAL(4, l1 | zen::at(-1));
+    ZEN_TEST_EQUAL(3, l1 | zen::at(-2));
+
+    boost::container::slist<int> s1 = boost::assign::list_of(9)(1)(2)(3)(4);
+
+    ZEN_TEST_EQUAL(9, s1 | zen::at(0));
+    ZEN_TEST_EQUAL(2, s1 | zen::at(2));
+    ZEN_TEST_EQUAL(4, s1 | zen::at(-1));
+    ZEN_TEST_EQUAL(3, s1 | zen::at(-2));
 }
 
 #endif
