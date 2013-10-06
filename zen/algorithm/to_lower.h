007e3814dfe753b0f4af44f1fe7666752f18156a
Paul Fultz II <pfultz2@yahoo.com>
2013-10-05 22:35:06 -0500

    Add to_lower

--- zen/algorithm/to_lower.h
+++ zen/algorithm/to_lower.h
@@ -8,13 +8,37 @@
 #ifndef ZEN_GUARD_ALGORITHM_TO_LOWER_H
 #define ZEN_GUARD_ALGORITHM_TO_LOWER_H
 
-namespace zen { namespace algorithm {
+#include <zen/algorithm/transform.h>
+#include <locale>
 
-class to_lower
+namespace zen { namespace detail {
+
+ZEN_FUNCTION_CLASS((to_lower_selector)(auto x)(std::tolower(x, std::locale())))
+
+}
+
+ZEN_FUNCTION_PIPE_OBJECT((to_lower)(auto r)
+    if (is_range<r>)(zen::transform(r, detail::to_lower_selector()))
+)
+
+}
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <zen/algorithm/equal.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <list>
+
+ZEN_TEST_CASE(to_lower_test)
 {
+    std::string s1 = "HELLO";
+    std::string s2 = "hello";
 
-};
+    ZEN_TEST_EQUAL(s2, s1 | zen::to_lower);
+    ZEN_TEST_EQUAL(s2, zen::to_lower(s1));
+}
 
-}}
+#endif
 
 #endif
