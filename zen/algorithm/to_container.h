575748c8265f3aa9b1b8be3c1c509a21376b2e51
Paul <pfultz2@yahoo.com>
2013-10-05 22:39:47 -0400

    Add initial to_container

--- zen/algorithm/to_container.h
+++ zen/algorithm/to_container.h
@@ -8,13 +8,56 @@
 #ifndef ZEN_GUARD_ALGORITHM_TO_CONTAINER_H
 #define ZEN_GUARD_ALGORITHM_TO_CONTAINER_H
 
-namespace zen { namespace algorithm {
+#include <zen/function/builder.h>
+#include <zen/traits.h>
+#include <zen/function/implicit.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
 
-class to_container
+namespace zen { namespace detail {
+
+template<class T>
+class to_container_f
 {
+    template<class X>
+    struct result;
 
+    template<class X, class Range>
+    struct result<X(Range)>
+    {
+        typedef T type;
+    };
+
+    template<class Range>
+    T operator()(const Range& r) const
+    {
+        return T(boost::begin(r), boost::end(r));
+    }
 };
+}
 
-}}
+ZEN_FUNCTION_PIPE_OBJECT((to_container)(auto r)
+        if (is_range<r>)(implicit<detail::to_container_f>()(r))
+    )
+}
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <zen/algorithm/equal.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <list>
+
+ZEN_TEST_CASE(to_container_test)
+{
+    std::vector<int> v1 = boost::assign::list_of(1)(2)(3);
+    std::list<int> l1 = zen::to_container(v1);
+    std::list<int> l2 = v1 | zen::to_container;
+    
+    ZEN_TEST_EQUAL(v1, l1);
+    ZEN_TEST_EQUAL(v1, l2);
+}
+
+#endif
 
 #endif
