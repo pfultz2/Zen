a7f51bf8dc29945904b9ae6fa5eca97ab4eff405
Paul Fultz II <pfultz2@yahoo.com>
2013-10-05 22:40:45 -0500

    Add tests for keys

--- zen/algorithm/keys.h
+++ zen/algorithm/keys.h
@@ -16,12 +16,28 @@ ZEN_FUNCTION_CLASS((keys_selector)(auto x)(x.first))
 
 }
 
-ZEN_FUNCTION_PIPE_OBJECT((keys)(r)
+ZEN_FUNCTION_PIPE_OBJECT((keys)(auto r)
     if (is_range_or_sequence<r>)(zen::transform(r, detail::keys_selector()))
 )
 
-
-
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <zen/algorithm/equal.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <map>
+
+ZEN_TEST_CASE(keys_test)
+{
+    std::map<int, int> m = boost::assign::map_list_of(0, 1)(1, 2)(2, 3);
+    std::vector<int> v = boost::assign::list_of(0)(1)(2);
+
+    ZEN_TEST_EQUAL(v, m | zen::keys);
+    ZEN_TEST_EQUAL(v, zen::keys(m));
+}
+
+#endif
 
 #endif
