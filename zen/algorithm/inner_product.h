393116bfbba013ac3637db893d599da10289c672
Paul Fultz II <pfultz2@yahoo.com>
2013-10-12 19:42:59 -0500

    Add tests for inner_product

--- zen/algorithm/inner_product.h
+++ zen/algorithm/inner_product.h
@@ -23,8 +23,35 @@ ZEN_FUNCTION_PIPE_OBJECT((inner_product)(auto r1, auto r2, x)
     (
         std::inner_product(boost::begin(r1), boost::end(r1), boost::begin(r2), x)
     )
+    def(auto r1, auto r2, x, op1, op2)
+    if (is_range<r1>, is_range<r2>)
+    (
+        std::inner_product(boost::begin(r1), boost::end(r1), boost::begin(r2), x, op1, op2)
+    )
 
 )
 }
+
+#ifdef ZEN_TEST
+#include <zen/test.h>
+#include <boost/assign.hpp>
+#include <vector>
+#include <utility>
+
+#include <iostream>
+
+ZEN_TEST_CASE(inner_product_test)
+{
+    std::vector<int> a = boost::assign::list_of(0)(1)(2)(3)(4);
+    std::vector<int> b = boost::assign::list_of(5)(4)(2)(3)(1);
+
+    ZEN_TEST_EQUAL(zen::inner_product(a, b, 0), 21);
+    ZEN_TEST_EQUAL(zen::inner_product(a, b, 0, std::plus<int>(), std::equal_to<int>()), 2);
+
+    ZEN_TEST_EQUAL(a | zen::inner_product(b, 0), 21);
+    ZEN_TEST_EQUAL(a | zen::inner_product(b, 0, std::plus<int>(), std::equal_to<int>()), 2);
+}
+
+#endif
 
 #endif
