7b39e11b3f89eca70f3bda0fc44cd5e09e470744
Paul Fultz II <pfultz2@yahoo.com>
2013-10-17 17:05:46 -0500

    Add missing header

---
+++ zen/algorithm/detail/default_equal.h
@@ -0,0 +1,19 @@
+/*=============================================================================
+    Copyright (c) 2013 Paul Fultz II, Neil Groves
+    default_equal.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZEN_GUARD_ALGORITHM_DETAIL_DEFAULT_EQUAL_H
+#define ZEN_GUARD_ALGORITHM_DETAIL_DEFAULT_EQUAL_H
+
+#include <zen/function/builder.h>
+
+namespace zen { namespace detail {
+
+ZEN_FUNCTION_CLASS((default_equal)(const x, const y)(x == y));
+
+}}
+
+#endif
