f2c59ee2261f4fb9eac5ed65923e8913e7ef8336
Paul <pfultz2@yahoo.com>
2014-05-04 21:02:09 -0400

    Fix some problems with iterator traits and add more tests

--- zen/iterator/iterator_traits.h
+++ zen/iterator/iterator_traits.h
@@ -18,65 +18,69 @@
 
 namespace zen {
 
-template<class T, class X = void>
-struct has_iterator_category 
-: std::false_type
+namespace detail {
+
+template<class Iterator>
+struct iterator_traits
+: std::iterator_traits<bare_t<Iterator>>
 {};
+
+template<class T>
+struct iterator_traits<const T*>
+: std::iterator_traits<const T*>
+{};
+
+}
+
+ZEN_TRAIT(has_iterator_category)
+{
+    template<class I>
+    auto requires(const I&) -> ZEN_VALID_EXPR(
+        zen::has_type<typename I::iterator_category>(),
+        zen::has_type<typename I::reference>(),
+        zen::has_type<typename I::pointer>(),
+        zen::has_type<typename I::value_type>(),
+        zen::has_type<typename I::difference_type>()
+    );
+};
 
 template<class T>
 struct has_iterator_category<T*>
 : std::true_type
 {};
 
-template<class Iterator>
-struct has_iterator_category<Iterator, typename zen::traits_detail::holder
-<
-    typename Iterator::iterator_category, 
-    typename Iterator::reference, 
-    typename Iterator::pointer, 
-    typename Iterator::value_type, 
-    typename Iterator::difference_type 
->::type >
-: std::true_type
-{};
-
 template<class T, class = ZEN_CLASS_REQUIRES(has_iterator_category<T>())>
 struct iterator_category
 {
-    typedef typename std::remove_reference<T>::type iterator_type;
-    typedef typename std::iterator_traits<iterator_type>::iterator_category type;
+    typedef typename detail::iterator_traits<T>::iterator_category type;
 };
 
 template<class T, class = ZEN_CLASS_REQUIRES(has_iterator_category<T>())>
 struct iterator_reference
 {
-    typedef typename std::remove_reference<T>::type iterator_type;
-    typedef typename std::iterator_traits<iterator_type>::reference type;
+    typedef typename detail::iterator_traits<T>::reference type;
 };
 
 template<class T, class = ZEN_CLASS_REQUIRES(has_iterator_category<T>())>
 struct iterator_pointer
 {
-    typedef typename std::remove_reference<T>::type iterator_type;
-    typedef typename std::iterator_traits<iterator_type>::pointer type;
+    typedef typename detail::iterator_traits<T>::pointer type;
 };
 
 template<class T, class = ZEN_CLASS_REQUIRES(has_iterator_category<T>())>
 struct iterator_value
 {
-    typedef typename std::remove_reference<T>::type iterator_type;
-    typedef typename std::iterator_traits<iterator_type>::value_type type;
+    typedef typename detail::iterator_traits<T>::value_type type;
 };
 
 template<class T, class = ZEN_CLASS_REQUIRES(has_iterator_category<T>())>
 struct iterator_difference
 {
-    typedef typename std::remove_reference<T>::type iterator_type;
-    typedef typename std::iterator_traits<iterator_type>::difference_type type;
+    typedef typename detail::iterator_traits<T>::difference_type type;
 };
 
 ZEN_TRAIT(is_iterator, 
-    is_semi_regular<_>)
+    is_semi_regular<_>, has_iterator_category<_>)
 {
     template<class I>
     auto requires(I&& i) -> ZEN_VALID_EXPR(
@@ -97,21 +101,25 @@ ZEN_TRAIT(is_input_iterator,
 };
 
 ZEN_TRAIT(is_output_iterator, 
-    is_iterator<boost::mpl::_1>)
+    is_iterator<_1>)
 {
     template<class I, class T>
     auto requires(I&& i, T&& out) -> ZEN_VALID_EXPR(
-        zen::is_true<is_iterator<I>>(),
         *i = out,
         *i++ = out,
-        zen::returns< const _t<std::remove_reference<I>>& >(i++)
+        zen::returns< const bare_t<I>& >(i++)
+    );
+
+    template<class I>
+    auto requires(I&& i) -> ZEN_VALID_EXPR(
+        this->requires(std::forward<I>(i), std::declval< _t<iterator_value<I>> >())
     );
 };
 
 ZEN_TRAIT(is_const_iterator,
     is_regular<_>, is_input_iterator<_>)
 {
-    template<class I, class T>
+    template<class I>
     auto requires(I&& i) -> ZEN_VALID_EXPR(
         zen::is_true< std::is_convertible<_t<boost::iterator_traversal<I>>, boost::forward_traversal_tag> >(),
         zen::returns< _t<iterator_reference<I>> >(*i)
@@ -119,19 +127,14 @@ ZEN_TRAIT(is_const_iterator,
 };
 
 ZEN_TRAIT(is_mutable_iterator,
-    is_const_iterator<_>)
+    is_const_iterator<_>, is_output_iterator<_>)
 {
-    template<class I, class T>
-    auto requires(I&& i) -> ZEN_VALID_EXPR(
-        zen::is_true<is_const_iterator<I>>(),
-        zen::is_true< is_output_iterator<I, _t<iterator_value<I>>> >()
-    );
 };
 
 ZEN_TRAIT(is_reversible_iterator,
     is_input_iterator<_>)
 {
-    template<class I, class T>
+    template<class I>
     auto requires(I&& i) -> ZEN_VALID_EXPR(
         zen::returns< _t<std::remove_reference<I>>& >(--i),
         i--
@@ -141,7 +144,7 @@ ZEN_TRAIT(is_reversible_iterator,
 ZEN_TRAIT(is_advanceable_iterator,
     is_input_iterator<_>, is_totally_ordered<_>)
 {
-    template<class I, class T>
+    template<class I>
     auto requires(I&& i) -> ZEN_VALID_EXPR(
         zen::returns< _t<iterator_reference<I>> >(i[1]),
         zen::returns< _t<std::remove_reference<I>>& >(i += 1),
@@ -160,12 +163,16 @@ ZEN_TRAIT(is_advanceable_iterator,
 #include <zen/traits/trait_check.h>
 #include <vector>
 
+ZEN_TRAIT_CHECK(zen::has_iterator_category<int*>);
+ZEN_TRAIT_CHECK(zen::has_iterator_category<std::vector<int>::iterator>);
 ZEN_TRAIT_CHECK(zen::is_iterator<std::vector<int>::iterator>);
 ZEN_TRAIT_CHECK(zen::is_input_iterator<std::vector<int>::iterator>);
 ZEN_TRAIT_CHECK(zen::is_output_iterator<std::vector<int>::iterator, int>);
-// ZEN_TRAIT_CHECK(zen::is_mutable_iterator<std::vector<int>::iterator>);
-// ZEN_TRAIT_CHECK(zen::is_reversible_iterator<std::vector<int>::iterator>);
-// ZEN_TRAIT_CHECK(zen::is_advanceable_iterator<std::vector<int>::iterator>);
+ZEN_TRAIT_CHECK(zen::is_output_iterator<std::vector<int>::iterator>);
+ZEN_TRAIT_CHECK(zen::is_const_iterator<std::vector<int>::iterator>);
+ZEN_TRAIT_CHECK(zen::is_mutable_iterator<std::vector<int>::iterator>);
+ZEN_TRAIT_CHECK(zen::is_reversible_iterator<std::vector<int>::iterator>);
+ZEN_TRAIT_CHECK(zen::is_advanceable_iterator<std::vector<int>::iterator>);
 
 // ZEN_TRAIT_CHECK(zen::is_iterator<std::vector<int>::iterator&>);
 // ZEN_TRAIT_CHECK(zen::is_input_iterator<std::vector<int>::iterator&>);
