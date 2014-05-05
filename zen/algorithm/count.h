cacadfb0d0bcfae7a17adee82036f29ee6a7d776
Paul <pfultz2@yahoo.com>
2014-05-05 17:35:22 -0400

    Update count algorithm

--- zen/algorithm/count.h
+++ zen/algorithm/count.h
@@ -9,9 +9,7 @@
 #define ZEN_GUARD_ALGORITHM_COUNT_H
 
 #include <zen/function/builder.h>
-#include <zen/traits.h>
-#include <boost/range/begin.hpp>
-#include <boost/range/end.hpp>
+#include <zen/range/range_traits.h>
 
 #include <boost/fusion/algorithm/query/count.hpp>
 #include <boost/fusion/sequence/intrinsic/size.hpp>
@@ -19,51 +17,20 @@
 
 namespace zen {
 
-namespace detail {
 
+ZEN_FUNCTION_PIPE_OBJECT((count)(const auto& r, const auto& x)
+        if (_p<is_range_of>(r, x))(std::count(zen::begin(r), zen::end(r), x))
+        else if (_p<boost::fusion::traits::is_sequence>(r))(boost::fusion::count(r, x))
+        def(const auto& r)
+        if (_p<is_range>(r))(zen::distance(r))
+        else if (_p<boost::fusion::traits::is_sequence>(r))(boost::fusion::size(r))
 
-struct count
-{
-    typedef std::ptrdiff_t result_type;
-    template<class R>
-    ZEN_FUNCTION_REQUIRES(is_range<R>)
-    (std::ptrdiff_t) operator()(const R& r) const
-    {
-        // TODO: Create a distance that recognizes iterator_traversals
-        return std::distance(boost::begin(r), boost::end(r));
-    }
-
-    template<class R, class T>
-    ZEN_FUNCTION_REQUIRES(is_range<R>)
-    (std::ptrdiff_t) operator()(const R& r, const T& x) const
-    {
-        return std::count(boost::begin(r), boost::end(r), x);
-    }
-
-    template<class R>
-    ZEN_FUNCTION_REQUIRES(is_sequence<R>, exclude is_range<R>)
-    (std::ptrdiff_t) operator()(const R& r) const
-    {
-        return boost::fusion::size(r);
-    }
-
-    template<class R, class T>
-    ZEN_FUNCTION_REQUIRES(is_sequence<R>, exclude is_range<R>)
-    (std::ptrdiff_t) operator()(const R& r, const T& x) const
-    {
-        return boost::fusion::count(r, x);
-    }
-
-};
-}
-
-static_<pipable_adaptor<detail::count> > count = {};
+    )
 
 }
 
 #ifdef ZEN_TEST
 #include <zen/test.h>
-#include <boost/assign.hpp>
 #include <vector>
 #include <boost/fusion/container/vector.hpp>
 #include <zen/algorithm/detail/is_odd.h>
@@ -71,13 +38,15 @@ static_<pipable_adaptor<detail::count> > count = {};
 
 ZEN_TEST_CASE(count_test)
 {
-    std::vector<int> v = boost::assign::list_of(1)(2)(3);
+    std::vector<int> v = { 1, 2, 3 };
     
     ZEN_TEST_EQUAL(1, zen::count(v, 2));
     ZEN_TEST_EQUAL(3, zen::count(v));
+    ZEN_TEST_EQUAL(3, v | zen::count);
     
     ZEN_TEST_EQUAL(1, zen::count(boost::fusion::make_vector(1,2,3), 2));
     ZEN_TEST_EQUAL(3, zen::count(boost::fusion::make_vector(1,2,3)));
+    ZEN_TEST_EQUAL(3, boost::fusion::make_vector(1,2,3) | zen::count);
 }
 
 #endif
