000119720a36bf3761f1c998f80ddefcef92bd98
Paul <pfultz2@yahoo.com>
2014-05-15 20:13:12 -0400

    Udpate indirect

--- zen/algorithm/indirect.h
+++ zen/algorithm/indirect.h
@@ -10,14 +10,13 @@
 
 #include <zen/algorithm/transform.h>
 
-namespace zen { namespace detail {
+namespace zen { 
 
-ZEN_FUNCTION_CLASS((indirect_selector)(auto x)(*x))
-
-}
-
-ZEN_FUNCTION_PIPE_OBJECT((indirect)(auto r)
-    if (is_range_or_sequence<r>)(zen::transform(r, detail::indirect_selector()))
+ZEN_FUNCTION_PIPE_OBJECT((indirect)(auto&& r)
+    if (_p<is_range>(r) or _p<boost::fusion::traits::is_sequence>(r))
+    (
+        zen::transform(r, [](auto&& x) -> auto& { return *x; })
+    )
 )
 
 
@@ -25,18 +24,16 @@ ZEN_FUNCTION_PIPE_OBJECT((indirect)(auto r)
 
 #ifdef ZEN_TEST
 #include <zen/test.h>
-#include <boost/assign.hpp>
 #include <vector>
 #include <boost/fusion/container/vector.hpp>
-#include <zen/algorithm/equal.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 
 
 ZEN_TEST_CASE(indirect_test)
 {
-    std::vector<boost::shared_ptr<int> > v1 = boost::assign::list_of(boost::make_shared<int>(1))(boost::make_shared<int>(2));
-    std::vector<int> v2 = boost::assign::list_of(1)(2);
+    std::vector<boost::shared_ptr<int> > v1 = { boost::make_shared<int>(1), boost::make_shared<int>(2)};
+    std::vector<int> v2 = { 1, 2 };
 
     ZEN_TEST_EQUAL(v1 | zen::indirect, v2);
 }
